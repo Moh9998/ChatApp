6e3e8d314ab9594f3a3db6870170a0a2870055e4
MohammedHussain <68030846+Moh9998@users.noreply.github.com>
2024-06-04 09:07:50 +0300

    Add project files.

---
+++ ChatApp/ChatApp/Program.cs
@@ -0,0 +1,48 @@
+using ChatApp.Client.Pages;
+using ChatApp.Components;
+using ChatApp.HUbs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.MapHub<ChatHub>("/chathub");
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.UseCors("AllowAll");
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(ChatApp.Client._Imports).Assembly);
+
+app.Run();
