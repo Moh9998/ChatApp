21d3f36c75f5a7f855c0b0ed7a89a5409220fdec
MohammedHussain <68030846+Moh9998@users.noreply.github.com>
2024-06-05 11:09:16 +0300

    creating project

--- ChatApp/ChatApp/HUbs/ChatHub.cs
+++ ChatApp/ChatApp/HUbs/ChatHub.cs
@@ -4,10 +4,20 @@ namespace ChatApp.HUbs
 {
     public class ChatHub    : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message, string room)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
         }
-        // Adding Rooms
+
+        public async Task JoinRoom(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+        }
+
+        public async Task LeaveRoom(string room)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, room);
+        }
+
     }
 }
---
+++ ChatApp/ChatApp/HUbs/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.HUbs
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public string Color { get; set; }
+    }
+}
